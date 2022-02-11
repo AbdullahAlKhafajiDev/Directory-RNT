f5a8ce64b693a3f64944f04d673b6cde697510dd
Abdullah Al Khafaji <57965732+BraveOPotato@users.noreply.github.com>
2022-02-11 10:42:21 -0600

    Add files via upload

---
+++ Directory RT 2.0.py
@@ -0,0 +1,22 @@
+import os
+space = ""
+print(space)
+Directory = os.getcwd()
+FilesInDirectory = os.listdir(Directory)
+PythonFile = os.path.basename(__file__)
+FilesInDirectory.remove(PythonFile)
+print(FilesInDirectory)
+print(space)
+x = input("What # do you want to start from? ")
+FileName = str(input("Name for the files? "))
+for file in FilesInDirectory:
+    FileExtension = os.path.splitext(file)
+    FileExtension = FileExtension[1]
+    print(FileExtension)
+    x = str(x)
+    print(Directory + file)
+    os.rename(Directory + '\\' + file, Directory + "\\" + FileName + " " + x + FileExtension)
+    x = int(x)
+    x +=1
+    print (file)
+
